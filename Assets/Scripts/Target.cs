df422e56e434f3caab3258f190d1ecdec449b705
Mjones090 <145160703+Mjones090@users.noreply.github.com>
2023-11-08 20:41:38 +0000

    Keeping score

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    public int pointValue;
     
 
 
@@ -37,7 +38,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
